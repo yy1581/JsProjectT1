f0ec572ff18931d041f159c66e39970ed74241df
yy1581 <148043845+yy1581@users.noreply.github.com>
2024-07-26 21:26:44 +0900

    login

---
+++ login.js
@@ -0,0 +1,36 @@
+const id = document.querySelector("#id");
+const loginForm = document.querySelector("#loginForm");
+const loginBtn = document.querySelector("#loginBtn");
+const logoutBtn = document.querySelector("#logoutBtn");
+const loginInfo = document.querySelector("#loginInfo");
+const userInfo = document.querySelector("#userInfo");
+
+function login(e){
+  e.preventDefault();
+  if(id.value === ""){
+    alert("id를 입력해주세요.");
+    return;
+  }
+  localStorage.setItem("id", id.value);
+  loginForm.classList.add("hidden");
+  loginInfo.classList.remove("hidden");
+  userInfo.innerHTML = `${localStorage.getItem("id")}님 환영합니다.`;
+}
+
+function logout(){
+  localStorage.removeItem("id");
+  loginForm.classList.remove("hidden");
+  loginInfo.classList.add("hidden");
+}
+
+function chkId(e){
+  if(localStorage.getItem("id") !== undefined){
+    loginForm.classList.add("hidden");
+    loginInfo.classList.remove("hidden");
+    userInfo.innerHTML = `${localStorage.getItem("id")}님 환영합니다.`;
+  }
+}
+
+chkId();
+loginBtn.addEventListener("click", login);
+logoutBtn.addEventListener("click", logout);
